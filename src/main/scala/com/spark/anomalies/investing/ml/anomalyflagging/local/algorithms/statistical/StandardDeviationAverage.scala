f31c73f2b307b19ffefc89fc42e650243a343f16
Alexis Leveratto <alexis.leveratto@gmail.com>
2021-05-17 15:52:37 +0100

    Add Standard Deviation from Average

---
+++ src/main/scala/com/spark/anomalies/investing/ml/anomalyflagging/local/algorithms/statistical/StandardDeviationAverage.scala
@@ -0,0 +1,35 @@
+package com.spark.anomalies.investing.ml.anomalyflagging.local.algorithms.statistical
+
+import breeze.linalg._
+import breeze.numerics.abs
+import breeze.stats.{stddev, mean => Bmean}
+import com.spark.anomalies.investing.ml.anomalyflagging.dto.DetectorOutput
+
+/** Class that implements Standard Deviation from the Average for Anomaly Detection
+ * @param cutoff cutoff value that is the bases of the evaluation
+ */
+class StandardDeviationAverage(cutoff: Double) extends StatisticalDetector {
+
+  override protected val detectorName: String = "StandardDeviationAverage"
+
+  /** Method that returns the mean and standard deviation of a time-series
+   * @param dv a dense vector
+   * @return the mean and standard deviation of the input vector
+   */
+  override protected def fit(dv: DenseVector[Double]): (Double, Double) = {
+    val mean = Bmean(dv(0 to -2))
+    val std = stddev(dv(0 to -2))
+
+    (mean, std)
+
+  }
+
+  override def anomalyScoring(dv: DenseVector[Double]): DetectorOutput = {
+    val (mean, std) = fit(dv)
+    val statistic: Double = abs(dv(-1) - mean)
+    val cut = std *:* cutoff
+
+    DetectorOutput(statistic > cut, getDetectorName())
+  }
+
+}
---
+++ src/test/scala/com/spark/anomalies/investing/ml/anomalyflagging/local/algorithms/statistical/StandardDeviationAverageTest.scala
@@ -0,0 +1,5 @@
+package com.spark.anomalies.investing.ml.anomalyflagging.local.algorithms.statistical
+
+class StandardDeviationAverageTest {
+
+}
